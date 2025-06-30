b6825d94d3086f583a6f8d0770a2b9203d1dd777
ZiyuWang0113 <ziw0372@gmail.com>
2025-06-30 05:49:55 -0400

    init commit

---
+++ src/Todo.tsx
@@ -0,0 +1,27 @@
+import {useState } from 'react';
+
+export default function Todo() {
+    const [todos, setTodos] = useState(['1', '2', '3', '4']);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleAdd = () => {
+        const newTodos = [...todos, inputValue] // spread operator
+        setTodos(newTodos);
+        setInputValue(''); // clear input after adding
+    }
+    return (
+        <div>
+            <h1>Todo</h1>
+            <input value={inputValue} 
+            onChange={e => setInputValue(e.target.value)}/>
+            <button onClick={handleAdd}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
